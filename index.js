fff7b5c141030bf76b62d88ed06231673cb2a7e3
stacey <staceyamarks@gmail.com>
2018-01-01 16:45:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+  var result = []
+  for(var i = 0; i < musicians.length; i++){
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return result
+}
+
+function johnLennonFacts(factsArray){
+  var result = []
+  var i = 0
+  while(i < factsArray.length){
+    result.push(`${factsArray[i]}!!!`)
+    i++
+  }
+return result
+}
+
+function iLoveTheBeatles(num){
+  var result = []
+  do{
+    result.push("I Love The Beatles!")
+    
+  } while(num < 15)
+}
